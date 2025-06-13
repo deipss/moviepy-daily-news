a3c03717429b0873784a69f10c81817c2653490f
deipss <deipss666@gmail.com>
2025-06-13 21:44:25 +0800

    data

--- video_generator.py
+++ video_generator.py
@@ -31,11 +31,7 @@ import time
 REWRITE = False
 EVENING = False
 
-hint_information = """信息来源: 
-1. chinadaily.com.cn [中国日报国际版] 
-2. https://www.aljazeera.com [中东半岛电视台]
-3. https://www.bbc.com [英国广播公司] 
-4. https://www.rt.com/ [今日俄罗斯电视台]"""
+hint_information = """信息来源: 1. chinadaily.com.cn [中国日报国际版] 2. https://www.aljazeera.com [中东半岛电视台] 3. https://www.bbc.com [英国广播公司] 4. https://www.rt.com/ [今日俄罗斯电视台]"""
 
 
 def build_today_introduction_path(today=datetime.now().strftime("%Y%m%d")):
