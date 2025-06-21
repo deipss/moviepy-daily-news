8e5fa1e70382301dcb50617148902ea757acfe73
deipss <deipss666@gmail.com>
2025-06-21 09:10:58 +0800

    add arrow head

--- video_generator.py
+++ video_generator.py
@@ -398,19 +398,32 @@ def combine_videos_with_transitions(video_paths, output_path):
     # final_clip.preview()
     return duration_list
 
+
 def add_walking_man(path, walk_video_path, duration_list):
     origin_v = VideoFileClip(path)
     all_duration = origin_v.duration
     duration_width_list = [duration / all_duration * GLOBAL_WIDTH for duration in duration_list]
     duration_width_int_list = [int(sum(duration_width_list[:i])) for i in range(1, len(duration_width_list))]
+    duration_width_number_list = [int(sum(duration_width_list[:i]) - duration_width_list[i-1] / 2) for i in
+                                  range(1, len(duration_width_list))]
     seg_clips = []
-    for seg in duration_width_int_list:
+    for idx, (seg, num) in enumerate(zip(duration_width_int_list, duration_width_number_list)):
         tag = ImageClip('videos/seg.png')
         tag = tag.resized(GAP / 2 / tag.h)
         tag = tag.with_position((seg, 'bottom')).with_duration(origin_v.duration).with_start(0)
         seg_clips.append(tag)
+
+        txt_clip = TextClip(
+            text=str(idx+1),
+            font_size=int(tag.h * 0.95),
+            color='white',
+            font='./font/simhei.ttf',
+            stroke_color='white',
+            stroke_width=1
+        ).with_duration(origin_v.duration).with_position((num, 'bottom')).with_start(0)
+        seg_clips.append(txt_clip)
     width = origin_v.w
-    walk = ImageClip('videos/process_panda.png')
+    walk = ImageClip('videos/arrowhead.png')
     walk = walk.resized(GAP / 2 / walk.h)
     walk = walk.with_position(lambda t: (t / origin_v.duration * width, 'bottom')).with_duration(
         origin_v.duration).with_start(0)
@@ -418,6 +431,7 @@ def add_walking_man(path, walk_video_path, duration_list):
     seg_clips.append(walk)
     video_with_bg = CompositeVideoClip(seg_clips, use_bgclip=True)
     video_with_bg = video_with_bg.with_audio(origin_v.audio)
+    # video_with_bg.preview()
     video_with_bg.write_videofile(walk_video_path, codec="libx264", audio_codec="aac", fps=FPS)
 
 
@@ -437,7 +451,7 @@ def combine_videos(today: str = datetime.now().strftime("%Y%m%d")):
     final_path_walk = build_today_final_video_walk_path(today)
     logger.info(f"主视频保存在:{final_path} and {final_path_walk}")
     duration_list = combine_videos_with_transitions(video_paths, final_path)
-    add_walking_man(final_path, final_path_walk,duration_list)
+    add_walking_man(final_path, final_path_walk, duration_list)
     end_time = time.time()  # 结束计时
     elapsed_time = end_time - start_time
     logger.info(f"生成新闻JSON文件...")
