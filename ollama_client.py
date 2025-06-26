5981b78fab47431c54a11dbc41dc303752b95523
deipss <deipss666@gmail.com>
2025-06-26 10:20:51 +0800

    remove all max token

--- ollama_client.py
+++ ollama_client.py
@@ -13,6 +13,7 @@ def timeit(func):
         elapsed_time = time.time() - start_time
         logger.info(f" Ollama函数 {func.__name__} 耗时 {elapsed_time:.4f} 秒")
         return result
+
     return wrapper
 
 
@@ -88,10 +89,10 @@ class OllamaClient:
         """
         prompt = f"请为以下文本生成一个简洁的中文新闻摘要（不超过{max_tokens}个字）：\n{text}"
         cnt = 3
-        response = self._generate_text(prompt, model, {"max_tokens": max_tokens})
+        response = self._generate_text(prompt, model)
         while cnt > 0:
             if "error" in response:
-                response = self._generate_text(prompt, model, {"num_predict": max_tokens})
+                response = self._generate_text(prompt, model )
                 logger.error(f"生成摘要失败,last time is {cnt}: {response['error']},text={text}")
             cnt -= 1
         summary = response.get("response", "")
@@ -101,7 +102,7 @@ class OllamaClient:
             logger.info(f"当前摘要={summary}")
             logger.info(f"当前摘要={len(summary)},摘要超过{max_tokens}个字，再次生成摘要")
             prompt = f"请为以下文本生成一个简洁的中文新闻摘要（不超过{max_tokens}个字）：\n{summary}"
-            response = self._generate_text(prompt, model, {"num_predict": max_tokens})
+            response = self._generate_text(prompt, model )
             summary = response.get("response", "")
             summary = self._extract_think(summary)
 
@@ -130,10 +131,10 @@ class OllamaClient:
 
         prompt = f"请为以下文本生成一个简洁的中文新闻摘要（不超过{max_tokens}个字）：\n{text}"
         cnt = 3
-        response = self._generate_text(prompt, model, {"num_predict": max_tokens})
+        response = self._generate_text(prompt, model )
         while cnt > 0:
             if "error" in response:
-                response = self._generate_text(prompt, model, {"num_predict": max_tokens})
+                response = self._generate_text(prompt, model )
                 logger.error(f"生成摘要失败,last time is {cnt}: {response['error']},text={text}")
             cnt -= 1
         summary = response.get("response", "")
@@ -143,7 +144,7 @@ class OllamaClient:
             logger.info(f"当前摘要={summary}")
             logger.info(f"当前摘要={len(summary)},摘要超过{max_tokens}个字，再次生成摘要")
             prompt = f"请为以下文本生成一个简洁的中文新闻摘要（不超过{max_tokens}个字）：\n{summary}"
-            response = self._generate_text(prompt, model, {"num_predict": max_tokens})
+            response = self._generate_text(prompt, model )
             summary = response.get("response", "")
             summary = self._extract_think(summary)
 
@@ -151,7 +152,7 @@ class OllamaClient:
 
     def generate_top_topic(self, text: str, model: str = "deepseek-r1:8b", max_tokens: int = 50) -> str:
         prompt = f"请从以下新闻主题，提取出影响力最高的4个，这4个主题每个主题再精简到10个字左右，同时请排除一些未成年内容,只需返回按序号排列4个主题：\n{text}"
-        response = self._generate_text(prompt, model, {"num_predict": max_tokens})
+        response = self._generate_text(prompt, model )
         summary = response.get("response", "")
         summary = self._extract_think(summary, is_replace_line=False)
         summary = summary.replace("**", "")
@@ -159,7 +160,7 @@ class OllamaClient:
             logger.info(f"当前主题={summary}")
             logger.info(f"当前主题={len(summary)},主题超过{max_tokens}个字，再次生成主题")
             prompt = f"请从以下新闻主题，提取出影响力最高的4个，这4个主题每个主题再精简到8个字左右，同时请排除一些未成年内容，只需返回按序号排列4个主题：：\n{summary}"
-            response = self._generate_text(prompt, model, {"num_predict": max_tokens // 10 * 9})
+            response = self._generate_text(prompt, model )
             summary = response.get("response", "")
             summary = self._extract_think(summary, is_replace_line=False)
             summary = summary.replace("**", "")
@@ -168,7 +169,7 @@ class OllamaClient:
     def generate_top_title(self, text: str, model: str = "deepseek-r1:8b",
                            max_tokens: int = 80, count: int = 15) -> str:
         prompt = f"请从以下带有序号的新闻主题，提取出影响力最高的{count}个，过滤一些区县市的新闻，最终返回影响力最高的新闻的原始序号（用英文逗号隔开）：\n{text}"
-        response = self._generate_text(prompt, model, {"num_predict": max_tokens})
+        response = self._generate_text(prompt, model )
         summary = response.get("response", "")
         summary = self._extract_think(summary, is_replace_line=False)
         summary = summary.replace("**", "")
@@ -177,7 +178,7 @@ class OllamaClient:
     def generate_top_news_summary(self, text: str, model: str = "deepseek-r1:8b",
                                   max_tokens: int = 80, count: int = 15) -> str:
         prompt = f"请从以下标题信息，生成一从在{max_tokens}左右的新闻稿：\n{text}"
-        response = self._generate_text(prompt, model, {"num_predict": max_tokens})
+        response = self._generate_text(prompt, model )
         summary = response.get("response", "")
         summary = self._extract_think(summary, is_replace_line=False)
         summary = summary.replace("**", "")
@@ -199,6 +200,6 @@ class OllamaClient:
 if __name__ == '__main__':
     client = OllamaClient()
     a = client.generate_summary_cn("""
-    \"Did you know I can run 100 metres in 19 seconds?\" Rod Stewart,SirRod Stewart, is boasting about his physical prowess. And why not? At the age of 80, he's still cavorting around the world, playing sold out shows, recording new music and even writing a book about his beloved model train set. This weekend, he'll play the coveted \"legends\" slot on Glastonbury's Pyramid stage... although the former headliner isn't 100% happy about his billing. \"I just wish they wouldn't call it the tea time slot,\" he complains. \"That sounds like pipe and slippers, doesn't it?\" He's also persuaded organisers to extend his set, securing an hour-and-a-half slot after initially being offered 75 minutes. \"Usually I do well over two hours so there's still a load of songs we won't be able to do,\" he says. \"But we've been working at it. I'm not gonna make any announcements between songs. I'll do one number, shout 'next', and go straight into the next one. \"I'm going to get in as many songs I can.\" It's not like he's short of choice. Sir Rod has one of the all-time classic songbooks, from early hits with the Faces such as Stay With Me and Ooh La La, to his solo breakthrough with Maggie May, the slick pop of Do Ya Think I'm Sexy? and his reinvention as a crooner on songs like Downtown Train and Have I Told You Lately. The last time he played Glastonbury, in 2002, he was viewed as an interloper – sitting awkwardly on the bill beside the likes of The White Stripes, Coldplay and Orbital. At first, \"the crowd was wary\" of the musician, who \"looked to be taking himself too seriously\", said the BBC's Ian Youngs in a review of the show. But a peerless setlist of singalongs won them over. By the end of the night, 100,000 people were swaying in time to Sailing as if they were genuinely adrift on the surging tides of the Atlantic. Amazingly, Rod has no memory of it. \"I don't remember a thing,\" he confesses. \"I do so many concerts, they all blend into one.\" One particular show does stand out, though. On New Year's Eve 1994, Sir Rod played a free gig on Brazil's Copacabana Beach, drawing a crowd of more than three million people. But it wasn't the record-breaking audience that made it memorable. \"I was violently sick about an hour before I was supposed to go on,\" he confesses. \"I'd eaten something terrible, and I was in a toilet going, 'huerrrgurkurkbleaggggh' \"I didn't think I was going to make it but luckily they got a doctor to sort me out.\" We're talking to the star about a month before Glastonbury at the Devonshire, a relaxed, old-school boozer just off Picadilly Circus that's become the favoured haunt of everyone from Ed Sheeran to U2. It's a bit too early for a drink, though, so Sir Rod orders up a venti coffee, shooing away an over-eager assistant who attempts to stir in his sugar. He's dressed in a cream jacket and black jeans, which sit above the ankle to show off his box-fresh, zebra-striped trainers. His white shirt is unbuttoned far enough to display a diamond-encrusted necklace with the crest of his beloved football club, Celtic. And then there's the hair. A bleached blonde vista of windswept spikes, so famous that it earned a whole chapter in the singer's autobiography. Steve Marriott of The Small Faces once claimed that Sir Rod achieved this gravity-defying barnet by rubbing mayonnaise into his scalp, then rubbing it with a towel. This, says the musician, is utter \"bollocks\". \"Nah, nah, nah. I used to use sugared hot water, before the days of hair lacquer. And I couldn't afford hair lacquer, anyway.\" But what really sets Sir Rod apart is that voice. Raspy, soulful, raw and expressive, he's one of rock and roll's best interpretive singers. There's a reason why his covers of Cat Steven's First Cut Is The Deepest or Crazy Horse's I Don't Wanna Talk About It have eclipsed the originals. So it's a surprise to learn that he was discovered not for his vocals, but his harmonica skills. That fateful night in 1964, he'd been at a gig on Twickenham's Eel Pie Island, and was drunkenly playing the riff from Holwin' Wolf's Smokestack Lightnin' while he waited for the train home, when he was overheard by influential blues musician Long John Baldry. \"As he described it, he was walking along platform nine when he noticed this pile of rubble and clothes with a nose pointing out,\" Sir Rod recalls. \"And that was me playing harmonica.\" At the time, he \"wasn't so sure\" about his singing voice. But, with Baldry's encouragement, he started to develop his signature sound. \"I wanted to always sound like Sam Cooke and Otis Redding, so that's the way I went,\" he says.  \"I suppose I was trying to be different from anybody else.\" Sir Rod began his ascent to stardom with the Jeff Beck Group and the Faces, a boisterous blues-rock outfit heavily inspired by the Rolling Stones – both on and off the stage. They were regularly so drunk he'd forget the words to his own songs, he admits. In the US, the group received a 40-year ban from the Holiday Inn hotel chain after racking up a $11,000 bill (£8,000 – or £54,000 in 2025 money) for trashing their rooms. \"We only did it because the Holiday Inns would treat us so badly, like we were the scum of the earth,\" he says. \"So we'd get our own back by smashing the hotels up. One time we actually got a couple of spoons and chiselled through the walls to one another's rooms. \"But we used to book in as Fleetwood Mac, so they'd get the blame.\" How come he never succumbed to drink and drugs, like many of his contemporaries? \"I never was a really druggy person, because I played football all the time and I had to be match fit,\" he says. \"I would use the word dabble. I've dabbled in drugs, but not anymore.\" Perhaps a more destructive force was the singer's womanising. He wrote You're In My Heart for Bond girl Britt Ekland, but they split two years later, due to his persistent unfaithfulness. His marriage to Alana Stewart and relationship with model Kelly Emberg ended the same way. \"When it came to beautiful women, I was a tireless seeker of experiences,\" he wrote in his memoir. \"I didn't know how to resist. And also... I thought I could get away with it.\" He thought he'd settled down after marrying model Rachel Hunter in 1990, but she left him nine years later, saying she felt she had \"lost her identity\" in the relationship. The split hit Sir Rod hard. \"I felt cold all the time,\" he said. \"I took to lying on the sofa in the day, with a blanket over me and holding a hot water bottle against my chest. \"I knew then why they call it heartbroken: You can feel it in your heart. I was distracted, almost to the point of madness.\" However, since 2007, the star has been happily married to TV presenter / police constable Penny Lancaster, with the couple reportedly renewing their vows in 2023. Last week, they celebrated their 18th wedding anniversary with a trip on the Orient Express from Paris, where they met in 2005, to La Cervara in Portofino, where they held their wedding ceremony, in a medieval monastery. These days, Sir Rod says, family is his priority. \"I've got eight kids all together, so sometimes I'll wake up in the morning and see all these messages, Stewart, Stewart, Stewart, Stewart…  and it's all the kids. It's just gorgeous.\" His youngest, Aiden, is now 14, and becoming an historian of his dad's work. \"He's gone back and listened to everything I've done, bless him,\" says the star. \"He knows songs that I don't even remember recording!\" His Glastonbury appearance coincides with the release of a new greatest hits album – his 20th. (\"Is it really?\" gasps Sir Rod. \"Ohgawwwwd.\") So how does it feel to look back over those five decades of music? \"Oh, it's tremendous,\" he says. \"It's a feeling that you've done what you set out to do. \"I don't consider myself a particularly good songwriter,\" he adds. \"I struggle with it. It takes me ages to write a set of lyrics. \"So I don't think I'm a natural songwriter. I'm just a storyteller, that's all. A humble storyteller.\" Maybe – but this humble storyteller is going to draw a crowd of thousands when he plays the Pyramid Stage on Sunday afternoon. \"You know, it's wonderful,\" he concedes. \"I'll be in good voice. I'll enjoy myself. I don't care anymore what the critics think. \"I'm there to entertain my people.\" A wheelie bin to carry your tent to smiling faces in the crowd - the festival is now under way. Alison Howe credits a Northampton teacher for inspiring her to pursue a dream of working in music. The Indian megastar stands by Sardaar Ji 3 as political tensions threaten to overshadow its release. U2 bassist Adam Clayton delivered a reading at the funeral of Lord Henry Mount Charles in Slane. Sir Michael and Emily Eavis welcome hundreds of punters to \"the most joyful city in the UK\". Copyright 2025 BBC. All rights reserved.TheBBCisnot responsible for the content of external sites.Read about our approach to external linking.
+    Thousands of people have poured into Worthy Farm after the gates officially opened for the 2025 Glastonbury Festival. Co-founder Sir Michael Eavis and his daughter Emily Eavis, who now runs the festival, led the countdown shortly before 08:00 BST in front of many who slept outside the gate on Tuesday night. More than 200,000 people are set to descend on the site in the coming days ahead of the main festival programme launching on Friday. Speaking shortly before the gates opened, Ms Eavis told the BBC: \"It's been such a build-up this year, it's been an amazing amount of excitement.\" Ms Eavis said: \"We're all so looking forward to opening the gates and to be able to do it with my dad has been amazing. \"It's the best moment to let them all in and it's just such a joyful city, the most joyful city in the UK for the next five days.\" Hundreds of people arrived on Tuesday night, sleeping under the stars in queues in a bid to be the first on site. Among them were James Trusson, 31, from Ash, Somerset, Grace Ball, 29, from Bournemouth and Dan Mortimore, from Compton Dundon, Somerset, who made it to the front of the line for the second year in a row. Having put themselves in prime position for a top camping spot, Ms Ball said their plans for the rest of the day were to go \"back to the car for snacks, and then sleep\". \"I'll crack a beer I think,\" added Mr Trusson. Hundreds of people have got in touch with the BBC with photos and stories of travelling to the festival - whether that's a train into Castle Cary, a long coach journey or bybike. A coach full of Glastonbury-goers was sat on the hard shoulder of the M6 with a blown tyre, and Bobby told us he had broken down next to the A303 on his way to the festival. Many heading to the festival for the first time shared their excitement, while others said returning for the 13th time was \"pretty awesome\". We also spoke to Laurence, who saidhe quit his job to attend Glastonbury Festival because his leave request was denied. Apart from the expected traffic on the A361 between Glastonbury and Worthy Farm, the main travel routes to the festival have remained relatively clear throughout the day. While the main acts might not start performing until Friday, there is plenty for revellers to enjoy away from the music. There are performances at the circus and theatre fields, seaside entertainment on offer at \"Glastonbury-on-Sea\" and plenty of food and drink stalls. Follow BBC Somerset onFacebookandX. Send your story ideas to us on email or viaWhatsApp on 0800 313 4630. The Indian megastar stands by Sardaar Ji 3 as political tensions threaten to overshadow its release. U2 bassist Adam Clayton delivered a reading at the funeral of Lord Henry Mount Charles in Slane. The Working Class Hero exhibition opened at Birmingham's Museum and Art Gallery on Wednesday. The refurb of University of Wolverhampton at the Halls is shortlisted in the Acoustic Awards 2025. Dafydd Iwan, one of Wales' best known folk singers, says the situation is \"very worrying\". Copyright 2025 BBC. All rights reserved.TheBBCisnot responsible for the content of external sites.Read about our approach to external linking.
     """)
     print(a)
